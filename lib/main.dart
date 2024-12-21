abe29b85ccbe5b1f83e923c356df407072b59b14
Gabriel Rosa <gabokuck1990@gmail.com>
2024-12-20 20:42:03 -0600

    primer maqueta

---
+++ lib/main.dart
@@ -0,0 +1,444 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isCollapsed = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        home: Scaffold(
+          body: NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification notification) {
+              if (notification.metrics.axis == Axis.vertical) {
+                final bool shouldCollapse =
+                    notification.metrics.extentBefore > 50;
+                if (shouldCollapse != isCollapsed) {
+                  setState(() {
+                    isCollapsed = shouldCollapse;
+                  });
+                }
+              }
+              return false;
+            },
+            child: SafeArea(
+                child: CustomScrollView(slivers: [
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                // -------------------- Sección de dirección y carrito de compras
+                AdressAndShopingCart(),
+                // -------------------- fin Sección de dirección y carrito de compras
+              ])),
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: 60,
+                collapsedHeight: 100,
+                toolbarHeight: 60,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Search(
+                        isCollapsed: isCollapsed,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                // -------------------- secciones
+                Categories(),
+                // -------------------- fin secciones
+
+                // -------------------- sección promociones
+                Promotions(),
+                // -------------------- fin sección promociones
+              ])),
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: 60,
+                collapsedHeight: 60,
+                toolbarHeight: 60,
+                flexibleSpace: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 25),
+                      child: Text(
+                        'Productos',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SliverList(
+                  delegate: SliverChildListDelegate([
+                // -------------------- sección productos
+
+                ProductsList()
+                // -------------------- fin sección productos]))],
+              ])),
+            ])),
+          ),
+        ));
+  }
+}
+
+class Promotions extends StatelessWidget {
+  const Promotions({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 0, left: 20, right: 0),
+      child: SizedBox(
+        height: 233,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 4),
+              child: Text(
+                'Promociones',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  ItemPromociones(),
+                  ItemPromociones(),
+                  ItemPromociones(),
+                  ItemPromociones(),
+                  ItemPromociones(),
+                  ItemPromociones(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Categories extends StatelessWidget {
+  const Categories({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 20, left: 20, right: 0),
+      child: SizedBox(
+        height: 100,
+        width: double.infinity,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          children: [
+            ItemCategory(),
+            ItemCategory(),
+            ItemCategory(),
+            ItemCategory(),
+            ItemCategory(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Search extends StatelessWidget {
+  final bool isCollapsed;
+
+  const Search({
+    super.key,
+    required this.isCollapsed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+      child: SizedBox(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Buscar en tienda',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            isCollapsed
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Icon(Icons.shopping_cart),
+                  )
+                : SizedBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AdressAndShopingCart extends StatelessWidget {
+  const AdressAndShopingCart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+      child: SizedBox(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            child: Row(
+              children: [
+                Text(
+                  '26 de Enero de 1857',
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+                Icon(Icons.expand_more),
+              ],
+            ),
+          ),
+          Icon(Icons.shopping_cart),
+        ],
+      )),
+    );
+  }
+}
+
+class ProductsList extends StatelessWidget {
+  const ProductsList({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 0, left: 20, right: 20),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              children: [
+                ItemProducto(),
+                ItemProducto(),
+                ItemProducto(),
+                ItemProducto(),
+                ItemProducto(),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemProducto extends StatelessWidget {
+  const ItemProducto({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+              child: FlutterLogo(
+                size: 100,
+                style: FlutterLogoStyle.horizontal,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Titulo de la promoción',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 14),
+                      ),
+                      Text(
+                        '\$145.00',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                            fontSize: 13,
+                            height: 1.5),
+                      ),
+                      Text(
+                        '\$155.00',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemPromociones extends StatelessWidget {
+  const ItemPromociones({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: SizedBox(
+        width: 150,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+              ),
+              child: FlutterLogo(
+                size: 100,
+                style: FlutterLogoStyle.horizontal,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Titulo de la promoción',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 14),
+                      ),
+                      Text(
+                        '\$145.00',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                            fontSize: 13,
+                            height: 1.5),
+                      ),
+                      Text(
+                        '\$155.00',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.lineThrough,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemCategory extends StatelessWidget {
+  const ItemCategory({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Column(
+          children: [
+            FlutterLogo(
+              size: 50,
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              'Categoría',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

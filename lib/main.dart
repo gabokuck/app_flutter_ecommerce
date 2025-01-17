e2cddc82703caf7ed620372a65c81a7c8f3e6f3d
Gabriel Rosa <gabokuck1990@gmail.com>
2025-01-17 11:20:43 -0600

    integración de firebase core y analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:app_ventas/blocs_provider.dart';
 import 'package:app_ventas/config/config.dart';
+import 'package:app_ventas/firebase_options.dart';
 import 'package:app_ventas/service_locator.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,6 +12,9 @@ late SharedPreferences sharedPref;
 void main() async {
   await Environment.initEnvironment();
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   sharedPref = await SharedPreferences.getInstance();
   serviceLocatorInit();
   runApp(const BlocsProvider(child: MyApp()));
